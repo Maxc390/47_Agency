59f11a82d600bf8a5dc8bd660f648447ea74efdb
Maxc390 <maxchanzu@gmail.com>
2026-02-12 10:14:56 +0300

    added terms and privacy-policy pages to sitemap

--- prototype-2/src/app/sitemap.ts
+++ prototype-2/src/app/sitemap.ts
@@ -6,6 +6,8 @@ export default function sitemap(): MetadataRoute.Sitemap {
     { url: `${baseUrl}/about`, lastModified: new Date(), changeFrequency: 'monthly', priority: 0.8 },
     { url: `${baseUrl}/contact`, lastModified: new Date(), changeFrequency: 'monthly', priority: 0.8 },
     { url: `${baseUrl}/projects`, lastModified: new Date(), changeFrequency: 'yearly', priority: 0.5 },
-    // i'll Add other pages later, e.g. { url: `${baseUrl}/projects`, ... }
+    { url: `${baseUrl}/privacy-policy`, lastModified: new Date(), changeFrequency: 'yearly', priority: 0.5 },
+    { url: `${baseUrl}/terms`, lastModified: new Date(), changeFrequency: 'yearly', priority: 0.5 },
+// i'll Add other pages later, e.g. { url: `${baseUrl}/projects`, ... }
   ];
 }
